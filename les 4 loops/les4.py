705e934ad65d78c85aca551d00ab279ee4fbf367
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2022-04-04 14:22:54 +0200

    les 5 hw af

--- les 4 loops/les4.py
+++ les 4 loops/les4.py
@@ -2,15 +2,29 @@
 # Nu willen we fizzbuzz voor alle getallen van 1 tot en met 100 uitvoeren 
 # Met input zou dit te lang duren ;)
 # Schrijf dit nu met een loop, zodat dit automagisch gebeurt
+getal = 32
 
-# print alle even getallen vanaf 0 tot en met 42; gebruik hiervoor geen modulo! 
-
+for i in range(getal):
+    if i % 3 == 0 and getal % 5 == 0:
+        print('FIZZBUZZ')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
+# print alle even getallen vanaf 0 tot en met 42; gebruik hiervoor geen modulo!
+for getal in range(0, 43, 2):
+    print(getal)
 # Schrijf een loop die een driehoek van sterretjes print, zoals
 # *
 # * *
 # * * *
 # etc.
 # Doe dit tot 10 sterretjes.
+for stars in range(1, 11):
+    print('* ' * stars)
+print()
 
 # Laat je driehoek nu ook weer terug lopen.
 # *
@@ -20,10 +34,21 @@
 # *
 # Hint: gebruik twee for loops
 
+for stars in range(0, 11):
+    print('* ' * stars)
+for stars in range(9, 0, -1):
+    print('* ' * stars)
+
 # Voor een extra uitdaging bij deze opdracht kan je de driehoek ook zo maken (doe dit pas als je de rest af hebt. Let op: is erg lastig):
 #   *
 #  * *
 # * * *
+
+grootte = 10
+for i in range(grootte):
+    print((grootte - i) * ' ' + i * '* ')
+
+
 
 # Schrijf nu je eigen max algoritme; zoek het grootste getal uit onderstaande lijst
 # Je mag er vanuit gaan dat het kleinste getal nul is.
@@ -32,7 +57,16 @@
 # Maak een var_max met als waarde nul
 # Loop over de lijst heen
 # Als de waarde groter is dan var_max maak de variabele deze waarde
-# 
+#
 # Check of jouw max overeenkomt met de max van de python functie max()
 
-lijst = [1,3,4,1,123,0,5,1,32,1,1,3,5,1,12,3,101,10]
+
+lijst = [1, 3, 4, 1, 123, 0, 5, 1, 32, 1, 1, 3, 5, 1, 125, 3, 101, 10]
+
+max_value = 0
+for getal in lijst:
+    if getal > max_value:
+        max_value = getal
+        print(max_value)
+
+print(max_value)
--- les 5/les 5.py
+++ les 5/les 5.py
@@ -1,54 +1,110 @@
-# Bekijk je fizzbuzz van vorige week.
-# Nu willen we fizzbuzz voor alle getallen van 1 tot en met 100 uitvoeren
-# Met input zou dit te lang duren ;)
-# Schrijf dit nu met een loop, zodat dit automagisch gebeurt
-getal = 32
+# hidden.py geeft jullie een geheim nummer genaamd `secret_number`, dit is iedere keer dat je het runt anders. Schrijf een programma met een while loop welke de gebruiker 3 pogingen geeft om het getal te raden. Als de gebruiker het getal niet goed raad vertel dan of het groter of kleiner is dan het geheime getal
+# hint gebruik de if,elif,else constructie die we hebben geleerd
 
-for i in range(getal):
-    if i % 3 == 0 and getal % 5 == 0:
-        print('FIZZBUZZ')
-    elif i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
-    else:
-        print(i)
-# print alle even getallen vanaf 0 tot en met 42; gebruik hiervoor geen modulo!
-for i in range(40 + 1):
-    print(i)
-# Schrijf een loop die een driehoek van sterretjes print, zoals
-# *
-# * *
-# * * *
-# etc.
-# Doe dit tot 10 sterretjes.
-n = 10
-for i in range(1, 10):
-    print('*' * i)
-    for j in range(10, 0, -1):
-        print(i * "*")
+# tries = 0
+# while tries < 3:
+#     guess = int(input(f"gok {tries + 1}, gok een nummer tussen 0 en 10: "))
+#     if guess > secret_number:
+#         print("Het getal is kleiner dan je gok")
+#     elif guess < secret_number:
+#         print("Het getal is groter dan je gok")
+#     else:
+#         print("Goedzo")
+#         break
+#     tries = tries + 1
+# if tries == 3:
+#     print('you lose')
 
-# Laat je driehoek nu ook weer terug lopen.
-# *
-# * *
-# (.. meer sterren ..)
-# * *
-# *
-# Hint: gebruik twee for loops
+#      _  _  __
+#  ___(_)(_)/ _| ___ _ __ ___
+# / __| || | |_ / _ \ '__/ __|
+# | (__| || |  _|  __/ |  \__ \
+# \___|_|/ |_|  \___|_|  |___/
+#      |__/
 
-# Voor een extra uitdaging bij deze opdracht kan je de driehoek ook zo maken (doe dit pas als je de rest af hebt. Let op: is erg lastig):
-#   *
-#  * *
-# * * *
+# > Vraag een gebruiker d.m.v. een while loop om cijfers van zijn studenten (komma getallen van 1 t/m 10)
+# > Sla deze op in een lijst
+# > Als de gebruiker 0 invoert is hij klaar met de lijst.
+from random import shuffle
 
-# Schrijf nu je eigen max algoritme; zoek het grootste getal uit onderstaande lijst
-# Je mag er vanuit gaan dat het kleinste getal nul is.
+cijfers = []
+cijfer = -1.0
+
+while cijfer != 0.0:
+    cijfer = float(input('vul een cijfer in om de lijst te vullen\n'))
+    if cijfer < 0 or cijfer > 10:
+        print(f'ongeldige invoer voor {cijfer}')
+        continue
+    cijfers.append(cijfer)
+
+# Nu we een lijst van cijfers hebben kunnen we daar dingen voor berekenen
+# > Bereken met behulp van een for loop het aantal elementen in de lijst:
+# > maak een variabele voor de lengte
+lengte = 0
+
+for cijfer in cijfers:
+    lengte += 1
+
+# print(f'de lijst bevat {lengte} cijfers')
+
+
+# > Bereken door een for loop te gebruiken het gemiddelde van de cijfers:
+
+# > maak een variabele
+# > gebruik een for loop om daar ieder cijfer bij op te tellen
+# > als dat klaar is deel het door het aantal getallen in de lijst
+totaal = 0
+for cijfer in cijfers:
+    totaal += cijfer
+gemiddelde = totaal/lengte
+
+# > Bereken door één for loop te gebruiken de volgende drie dingen: hoeveel voldoendes er zijn, het minimum en het maximum in de lijst:
+# > Gebruik drie variabelen een voor de hoeveelheid voldoendes, een voor het minimum en een voor het maximum
+# > Gebruik in je for loop drie if statements (een voor het minimum en een voor het maximum)
+voldoendes = 0
+maximum = 0
+minimum = 11
+
+for cijfer in cijfers:
+    if cijfer < minimum:
+        minimum = cijfer
+    if cijfer > maximum:
+        maximum = cijfer
+    if cijfer >= 5.5:
+        voldoendes += 1
+
+print(f'aantal cijfers {lengte}')
+print(f'Het gemiddelde cijfer {gemiddelde}')
+print(f'Het minimum cijfer is {minimum} en het maximale cijfer: {maximum}')
+print(f'er zijn {voldoendes} voldoendes behaald')
+
+# > print nu de statistieken over deze cijfers die je net hebt berekend
+
+#      _            __  __ _
+#  ___| |__  _   _ / _|/ _| | ___
+# / __| '_ \| | | | |_| |_| |/ _ \
+# \__ \ | | | |_| |  _|  _| |  __/
+# |___/_| |_|\__,_|_| |_| |_|\___|
 #
-# Stappen:
-# Maak een var_max met als waarde nul
-# Loop over de lijst heen
-# Als de waarde groter is dan var_max maak de variabele deze waarde
-#
-# Check of jouw max overeenkomt met de max van de python functie max()
 
-lijst = [1, 3, 4, 1, 123, 0, 5, 1, 32, 1, 1, 3, 5, 1, 12, 3, 101, 10]
+# shuffle sort
+# shuffle sort is een van de minst efficiente sort algoritmen dat er is
+# het gaat als volgt te werk:
+# > Neem een lijst met getallen (bijvoorbeeld de lijst die je in de vorige opdracht hebt gemaakt)
+# > Shuffle deze random door elkaar (als het schudden van een pakje kaarten)
+# > Kijk of de lijst gesorteerd is
+# > Is de lijst gesorteerd ben je klaar, is deze niet gesorteerd shuffle dan nog een keer
+#
+# Wij hebben de lastigere onderdelen al geschreven voor jullie:
+# gebruik de volgende functie om te kijken of een lijst gesorteerd is
+# `is_sorted(lijst)` > Deze returnt True als de lijst gesorteerd is en False als dat niet het geval is.
+# gebruik deze functie om een lijst te schudden
+# `shuffle(lijst)` > deze verandert de bestaande lijst
+
+
+while not is_sorted(cijfers):
+    print(cijfers)
+    shuffle(cijfers)
+print(cijfers)
+#
+# Om te kijken hoe het sorteren gaat kan je tussendoor je lijst printen
